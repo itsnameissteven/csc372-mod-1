b508c49036646e12abaddd8325405f806481491b
Steven Mancine <steven.mancine@charter.com>
2024-04-20 13:42:01 -0600

    update and document bank account

--- src/main/BankAccount.java
+++ src/main/BankAccount.java
@@ -1,4 +1,5 @@
 package main;
+import java.text.NumberFormat;
 import java.util.Random;
 
 public class BankAccount {
@@ -7,12 +8,12 @@ public class BankAccount {
 	private int accountID;
 	private double balance;
 	
-	public BankAccount(String firstName, String lastName) {
+	BankAccount(String firstName, String lastName) {
 		Random random = new Random();
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.balance = 0.0;
-		// Generate random 9 digit int
+		// Generate random 9 digit int for account id
 		this.accountID = 100000000 + random.nextInt(900000000);
 	}
 	
@@ -29,29 +30,32 @@ public class BankAccount {
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
-	public void setBalance(double balance) {
-		this.balance = balance;
+	public void setAccountId(int accountID) {
+		this.accountID = accountID;
 	}
 	
 	// Methods 
+	// Will deposit if amount greater than 0 is provided.
 	public void deposit(double amount) {
 		if(amount <= 0.0) {
 			System.out.println("Deposit amount must be greater than 0.");
 			return;
 		}
 		this.balance += amount;
-		System.out.println("Your account balance is now $" + this.balance);
+		System.out.println("Your account balance is now $" + NumberFormat.getCurrencyInstance().format(this.balance));
 	}
 	
+	// Will withdraw if a balance greater than 0 is provided.
 	public void withdrawal(double amount) {
 		if(amount <= 0.0) {
 			System.out.println("Withdrawal amount must be greater than 0");
 			return;
 		}
 		this.balance -= amount;
-		System.out.println("Your account balance is now $" + this.balance);
+		System.out.println("Your account balance is now $" + NumberFormat.getCurrencyInstance().format(this.balance) );
 	}
 	
+	// Prints all account information
 	public void accountSummary() {
 		System.out.println("AccountId: " + this.accountID);
 		System.out.println("First name: " + this.firstName);
